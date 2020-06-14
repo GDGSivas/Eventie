c7fd28d03a8ee8ddb63686be3423664891fcff6a
Emre Dipi <mail@emredipi.com>
2020-06-14 16:36:36 +0300

    last changes

--- src/components/SidebarPlugin/SideBar.vue
+++ src/components/SidebarPlugin/SideBar.vue
@@ -11,20 +11,7 @@
             </router-link>
 
             <slot name="mobile-right">
-                <ul class="nav align-items-center d-md-none">
-                    <base-dropdown class="nav-item" position="right">
-                        <a slot="title" class="nav-link nav-link-icon" href="#" role="button" data-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">
-                            <i class="ni ni-bell-55"/>
-                        </a>
-
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </base-dropdown>
-
-                </ul>
+                <span style="width: 30px"/>
             </slot>
             <slot/>
             <div v-show="$sidebar.showSidebar" class="navbar-collapse collapse show" id="sidenav-collapse-main">
@@ -33,7 +20,7 @@
                     <div class="row">
                         <div class="col-6 collapse-brand">
                             <router-link to="/" class="navbar-brand">
-                                <img :src="logo" >
+                                <img :src="logo">
                             </router-link>
                         </div>
                         <div class="col-6 collapse-close">
@@ -46,48 +33,48 @@
                     <slot name="links">
                     </slot>
                 </ul>
-                
+
             </div>
-            </div>
+        </div>
     </nav>
 </template>
 <script>
-  import NavbarToggleButton from '@/components/NavbarToggleButton'
+	import NavbarToggleButton from '@/components/NavbarToggleButton'
 
-  export default {
-    name: 'sidebar',
-    components: {
-      NavbarToggleButton
-    },
-    props: {
-      logo: {
-        type: String,
-        default: 'https://gdgsivas.org/iwd2020/images/gdg.png',
-        description: 'Sidebar app logo'
-      },
-      autoClose: {
-        type: Boolean,
-        default: true,
-        description: 'Whether sidebar should autoclose on mobile when clicking an item'
-      }
-    },
-    provide() {
-      return {
-        autoClose: this.autoClose
-      };
-    },
-    methods: {
-      closeSidebar() {
-        this.$sidebar.displaySidebar(false)
-      },
-      showSidebar() {
-        this.$sidebar.displaySidebar(true)
-      }
-    },
-    beforeDestroy() {
-      if (this.$sidebar.showSidebar) {
-        this.$sidebar.showSidebar = false;
-      }
-    }
-  };
+	export default {
+		name: 'sidebar',
+		components: {
+			NavbarToggleButton
+		},
+		props: {
+			logo: {
+				type: String,
+				default: 'https://gdgsivas.org/iwd2020/images/gdg.png',
+				description: 'Sidebar app logo'
+			},
+			autoClose: {
+				type: Boolean,
+				default: true,
+				description: 'Whether sidebar should autoclose on mobile when clicking an item'
+			}
+		},
+		provide() {
+			return {
+				autoClose: this.autoClose
+			};
+		},
+		methods: {
+			closeSidebar() {
+				this.$sidebar.displaySidebar(false)
+			},
+			showSidebar() {
+				this.$sidebar.displaySidebar(true)
+			}
+		},
+		beforeDestroy() {
+			if (this.$sidebar.showSidebar) {
+				this.$sidebar.showSidebar = false;
+			}
+		}
+	};
 </script>
